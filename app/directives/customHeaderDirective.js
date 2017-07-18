49d2f0f0c69fa67c7b020dd714e04d55dee4f16e
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2017-07-18 09:44:01 -0500

    Created header directive

---
+++ app/directives/customHeaderDirective.js
@@ -0,0 +1,20 @@
+core.directive('customHeader', function ($controller) {
+    return {
+        templateUrl: 'views/directives/header.html',
+        restrict: 'E',
+        replace: false,
+        transclude: true,
+        scope: {
+          login: '&',
+          anonymous: '='
+        },
+        link: function ($scope, element, attr) {
+            $scope.title = attr.title;
+            $scope.home = attr.home;
+            $scope.loginuser = function() {
+              console.log('called');
+              $scope.login({});
+            }
+        }
+    };
+});
