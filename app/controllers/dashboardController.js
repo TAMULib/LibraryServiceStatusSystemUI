263ae2b1f04ae783887d8d1f5d07794a199abbd5
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2017-07-18 16:05:43 -0500

    Incorporated new fileter into dashboard view

--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -1,4 +1,4 @@
-app.controller("DashboardController", function($controller, $scope, AlertService, User, OverallStatusPublic, OverallStatusFull, Service, ServiceRepo, NoteRepo) {
+app.controller("DashboardController", function($controller, $scope, AlertService, User, OverallStatusPublic, OverallStatusFull, Service, ServiceRepo, NoteRepo, UserService) {
 
     angular.extend(this, $controller('AppAbstractController', {$scope: $scope}));
 
@@ -6,8 +6,23 @@ app.controller("DashboardController", function($controller, $scope, AlertService
 
     $scope.services = ServiceRepo.getAll();
 
+    $scope.showShortList = true;
+
     $scope.getNoteById = function(id) {
       return NoteRepo.findById(id);
     };
+
+    $scope.showPublic = function() {
+      var user = UserService.getCurrentUser();
+      var publicView = false;
+      if (user.role === 'ROLE_ANONYMOUS' || user.role === 'ROLE_USER') {
+        publicView = true;
+      } 
+      return publicView;
+    };
+
+    $scope.showHideShortList = function() {
+      $scope.showShortList = !$scope.showShortList;
+    }
 
 });
