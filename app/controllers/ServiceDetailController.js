047edc22094349057826058f59afd46e93f62ee8
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2017-07-20 16:29:36 -0500

    Finished some styling of notes

--- app/controllers/ServiceDetailController.js
+++ app/controllers/ServiceDetailController.js
@@ -1,7 +1,6 @@
 app.controller('ServiceDetailController', function($controller, $routeParams, $scope, Service, ServiceRepo) {
 
   angular.extend(this, $controller('AppAbstractController', {$scope: $scope}));
-  console.log($routeParams.serviceId);
 
   ServiceRepo.ready().then(function() {
     $scope.service = ServiceRepo.findById($routeParams.serviceId);
