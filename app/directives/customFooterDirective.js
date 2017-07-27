438e92588c76fb99d16e55bceb3a55fed181a841
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2017-07-27 08:15:15 -0500

    Created custom footer directive

---
+++ app/directives/customFooterDirective.js
@@ -0,0 +1,17 @@
+app.directive('customFooter', function ($controller) {
+    return {
+        templateUrl: 'views/directives/footer.html',
+        restrict: 'E',
+        replace: false,
+        transclude: true,
+        scope: {
+            login: '&',
+            anonymous: '='
+        },
+        link: function ($scope, element, attr) {
+            $scope.loginuser = function() {
+                $scope.login({});
+            }
+        }
+    };
+});
