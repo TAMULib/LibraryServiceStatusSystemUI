2d80f6db627ab86c7fb6227af849a15a565b2bc7
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2017-07-27 08:15:43 -0500

    Incorporated new footer

--- app/directives/customHeaderDirective.js
+++ app/directives/customHeaderDirective.js
@@ -1,19 +1,18 @@
-core.directive('customHeader', function ($controller) {
+app.directive('customHeader', function ($controller) {
     return {
         templateUrl: 'views/directives/header.html',
         restrict: 'E',
         replace: false,
         transclude: true,
         scope: {
-          login: '&',
-          anonymous: '='
+            login: '&',
+            anonymous: '='
         },
         link: function ($scope, element, attr) {
             $scope.title = attr.title;
             $scope.home = attr.home;
             $scope.loginuser = function() {
-              console.log('called');
-              $scope.login({});
+                $scope.login({});
             }
         }
     };
