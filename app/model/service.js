6aeb54c6bf70d03b43de7357f35d7d13952fc028
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2017-07-31 10:01:22 -0500

    Fixed minor logic issue

--- app/model/service.js
+++ app/model/service.js
@@ -5,27 +5,28 @@ app.model("Service", function Service($q, Note, NoteRepo) {
       var service = this;
         
       service.before(function() {
-        
-        if (service.notes.length > 0) {
+        if (service.notes) {
+          console.log(service);
+          if (service.notes.length > 0) {
 
-          var notePromises = []
+            var notePromises = []
 
-          for(var i in service.notes) {
-            var noteId = service.notes[i];
-            notePromises.push($q(function(resolve) { 
-              NoteRepo.fetchAndAddById(noteId).then(function(note) {
-                resolve(new Note(note));
+            for(var i in service.notes) {
+              var noteId = service.notes[i];
+              notePromises.push($q(function(resolve) { 
+                NoteRepo.fetchAndAddById(noteId).then(function(note) {
+                  resolve(new Note(note));
+                });
+              }));
+            }
+
+            $q.all(notePromises).then(function(serviceNotes) {
+              angular.extend(service, {
+                notes: serviceNotes
               });
-            }));
-          }
-
-          $q.all(notePromises).then(function(serviceNotes) {
-            angular.extend(service, {
-              notes: serviceNotes
             });
-          });
+          }
         }
-
       });
 
       return service;
