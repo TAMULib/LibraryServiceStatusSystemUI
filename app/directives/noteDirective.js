e7e79c69b52ff0581d056a1f82117b26c22ebe98
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2017-08-02 09:54:25 -0500

    Fixed a bug in the underscoreFilter and removed log statment

--- app/directives/noteDirective.js
+++ app/directives/noteDirective.js
@@ -3,7 +3,6 @@ app.directive("note", function() {
     templateUrl: 'views/directives/note.html',
     restrict: 'E',
     link: function($scope, element, attr) {
-      console.log(attr);
       $scope.link = attr.link;
     }
   };
