1e802a3253381f9f44f14585aef7790b8f91f1fc
William Welling <wwelling@library.tamu.edu>
2017-08-04 09:51:44 -0500

    Delete ServiceDetailController.js

--- app/controllers/ServiceDetailController.js
+++
@@ -1,9 +0,0 @@
-app.controller('ServiceDetailController', function($controller, $routeParams, $scope, Service, ServiceRepo) {
-
-  angular.extend(this, $controller('AppAbstractController', {$scope: $scope}));
-
-  ServiceRepo.ready().then(function() {
-    $scope.service = ServiceRepo.findById($routeParams.serviceId);
-  });
-
-});
