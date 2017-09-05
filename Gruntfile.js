e05ee34969840c00beb0ef3713510c65c1238dc8
William Welling <wwelling@library.tamu.edu>
2017-09-05 07:37:38 -0500

    improved grunt deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,13 +22,6 @@ module.exports = function (grunt) {
             }
         },
 
-        useminPrepare: {
-            html: '<%= build.app %>/index.html',
-            options: {
-                dest: '<%= build.app %>'
-            }
-        },
-
         jshint: {
             options: {
                 jshintrc: '.jshintrc',
@@ -51,28 +44,113 @@ module.exports = function (grunt) {
             options: {
                 separator: ';'
             },
-            core: {
+            vendor: {
                 src: [
-                    'node_modules/node_modules/**/*.js',
-                    '!node_modules/node_modules/app/core.js',
-                    '!node_modules/node_modules/config/coreConfig.js',
-                    '!node_modules/node_modules/components/**/*',
-                    '!node_modules/node_modules/resources/**/*'
+                    'node_modules/jquery/dist/jquery.min.js',
+                    'node_modules/bootstrap/dist/js/bootstrap.min.js',
+
+                    'node_modules/sockjs-client/dist/sockjs.min.js',
+                    'node_modules/stompjs/lib/stomp.min.js',
+
+                    'node_modules/angular/angular.min.js',
+
+                    'node_modules/angular-route/angular-route.min.js',
+                    'node_modules/angular-loader/angular-loader.min.js',
+                    'node_modules/angular-sanitize/angular-sanitize.min.js',
+                    'node_modules/angular-messages/angular-messages.min.js',
+                    'node_modules/angular-mocks/angular-mocks.js',
+
+                    'node_modules/ng-file-upload/dist/ng-file-upload-shim.min.js',
+                    'node_modules/ng-file-upload/dist/ng-file-upload.min.js',
+
+                    'node_modules/ng-table/bundles/ng-table.min.js',
+
+                    'node_modules/tinymce/tinymce.min.js',
+                    'node_modules/angular-ui-tinymce/dist/tinymce.min.js',
+
+                    'node_modules/angular-bootstrap/ui-bootstrap-tpls.min.js'
                 ],
-                dest: '<%= build.app %>/resources/scripts/core_concat.js'
+                dest: '<%= build.app %>/resources/scripts/vendor_concat.js'
+            },
+            weaver: {
+                src: [
+                    'node_modules/weaver-ui-core/app/config/coreConfig.js',
+
+                    'node_modules/weaver-ui-core/app/components/version/version.js',
+                    'node_modules/weaver-ui-core/app/components/version/version-directive.js',
+                    'node_modules/weaver-ui-core/app/components/version/interpolate-filter.js',
+
+                    'node_modules/weaver-ui-core/app/core.js',
+                    'node_modules/weaver-ui-core/app/setup.js',
+                    'node_modules/weaver-ui-core/app/config/coreRuntime.js',
+                    'node_modules/weaver-ui-core/app/config/coreAngularConfig.js',
+                    'node_modules/weaver-ui-core/app/config/logging.js',
+
+                    'node_modules/weaver-ui-core/app/directives/headerDirective.js',
+                    'node_modules/weaver-ui-core/app/directives/footerDirective.js',
+                    'node_modules/weaver-ui-core/app/directives/userDirective.js',
+                    'node_modules/weaver-ui-core/app/directives/modalDirective.js',
+                    'node_modules/weaver-ui-core/app/directives/alertDirective.js',
+                    'node_modules/weaver-ui-core/app/directives/accordionDirective.js',
+                    'node_modules/weaver-ui-core/app/directives/tabsDirective.js',
+
+                    'node_modules/weaver-ui-core/app/directives/tooltipDirective.js',
+                    'node_modules/weaver-ui-core/app/directives/validationMessageDirective.js',
+                    'node_modules/weaver-ui-core/app/directives/validatedInputDirective.js',
+                    'node_modules/weaver-ui-core/app/directives/validatedSelectDirective.js',
+                    'node_modules/weaver-ui-core/app/directives/validatedTextAreaDirective.js',
+
+                    'node_modules/weaver-ui-core/app/services/accesscontrollservice.js',
+                    'node_modules/weaver-ui-core/app/services/wsservice.js',
+                    'node_modules/weaver-ui-core/app/services/wsapi.js',
+                    'node_modules/weaver-ui-core/app/services/restapi.js',
+                    'node_modules/weaver-ui-core/app/services/authserviceapi.js',
+                    'node_modules/weaver-ui-core/app/services/storageservice.js',
+                    'node_modules/weaver-ui-core/app/services/utilityservice.js',
+                    'node_modules/weaver-ui-core/app/services/alertservice.js',
+                    'node_modules/weaver-ui-core/app/services/validationstore.js',
+                    'node_modules/weaver-ui-core/app/services/userservice.js',
+                    'node_modules/weaver-ui-core/app/services/modalservice.js',
+                    'node_modules/weaver-ui-core/app/services/modelcache.js',
+                    'node_modules/weaver-ui-core/app/services/modelupdateservice.js',
+
+                    'node_modules/weaver-ui-core/app/repo/abstractRepo.js',
+
+                    'node_modules/weaver-ui-core/app/model/abstractModel.js',
+                    'node_modules/weaver-ui-core/app/model/assumedControl.js',
+                    'node_modules/weaver-ui-core/app/model/user.js',
+
+                    'node_modules/weaver-ui-core/app/controllers/abstractController.js',
+                    'node_modules/weaver-ui-core/app/controllers/coreAdminController.js',
+                    'node_modules/weaver-ui-core/app/controllers/authenticationController.js',
+                    'node_modules/weaver-ui-core/app/controllers/loginController.js',
+                    'node_modules/weaver-ui-core/app/controllers/registrationController.js',
+                    'node_modules/weaver-ui-core/app/controllers/userController.js',
+                    'node_modules/weaver-ui-core/app/controllers/errorpagecontroller.js'
+                ],
+                dest: '<%= build.app %>/resources/scripts/weaver_concat.js'
             },
             angular: {
                 src: [
                     '<%= build.app %>/**/*.js',
-                    '!<%= build.app %>/config/appConfig.js',
-                    '!<%= build.app %>/config/appConfig_sample.js',
-                    '!node_modules/**/*',
-                    '!<%= build.app %>/node_modules/**/*',
-                    '!<%= build.app %>/components/**/*',
+                    '<%= build.app %>/config/appConfig.js',
+                    '<%= build.app %>/config/apiMapping.js',
+                    '<%= build.app %>/components/version/version.js',
+                    '<%= build.app %>/components/version/version-directive.js',
+                    '<%= build.app %>/components/version/interpolate-filter.js',
+                    '!<%= build.app %>/config/**/*',
                     '!<%= build.app %>/resources/**/*',
-                    '!<%= build.app %>/resources/scripts/app_concat.js'
+                    '!<%= build.app %>/components/**/*'
                 ],
                 dest: '<%= build.app %>/resources/scripts/app_concat.js'
+            },
+            bundle: {
+                src: [
+                    '<%= build.app %>/resources/scripts/vendor_concat.js',
+                    '<%= build.app %>/resources/scripts/weaver_concat.js',
+                    '<%= build.app %>/resources/scripts/app_concat.js'
+                ],
+                dest: '<%= build.app %>/resources/scripts/bundle.js'
             }
         },
 
@@ -80,13 +158,9 @@ module.exports = function (grunt) {
             options: {
                 mangle: false
             },
-            core: {
-                src: '<%= build.app %>/resources/scripts/core_concat.js',
-                dest: '<%= build.app %>/resources/scripts/core_concat.js'
-            },
-            angular: {
-                src: '<%= build.app %>/resources/scripts/app_concat.js',
-                dest: '<%= build.app %>/resources/scripts/app_concat.js'
+            bundle: {
+                src: '<%= build.app %>/resources/scripts/bundle.js',
+                dest: '<%= build.app %>/resources/scripts/bundle.js'
             }
         },
 
@@ -98,7 +172,7 @@ module.exports = function (grunt) {
         },
 
         sass: {
-        	options: {
+            options: {
                 sourceMap: false
             },
             dist: {
@@ -133,8 +207,8 @@ module.exports = function (grunt) {
 
     grunt.registerTask('watch', ['watch']);
 
-    grunt.registerTask('develop', ['jshint', 'useminPrepare', 'concat', 'usemin', 'watch']);
+    grunt.registerTask('develop', ['jshint', 'concat', 'usemin', 'watch']);
 
-    grunt.registerTask('deploy', ['jshint', 'useminPrepare', 'concat', 'uglify', 'usemin', 'sass']);
+    grunt.registerTask('deploy', ['jshint', 'concat', 'uglify', 'usemin', 'sass']);
 
 };
