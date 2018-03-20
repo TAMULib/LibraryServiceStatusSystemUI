06c7d71eda8503c34a25089c0064c02354e6ac3b
William Welling <wwelling@library.tamu.edu>
2018-03-20 13:59:25 -0500

    testing scope methods for create and update

--- tests/mocks/repo/mockServiceRepo.js
+++ tests/mocks/repo/mockServiceRepo.js
@@ -1,24 +1,116 @@
+var mockServices = [  
+  {
+    "notes":[
+
+    ],
+    "id":1,
+    "schedules":[
+
+    ],
+    "withinSchedule":false,
+    "name":"Test 1",
+    "aliases":[
+
+    ],
+    "status":"UP",
+    "isAuto":false,
+    "isPublic":true,
+    "onShortList":false,
+    "serviceUrl":null,
+    "description":"<p>Hello, Test 1!</p>",
+    "projectId":null,
+    "type":"service"
+  },
+  {
+    "notes":[
+
+    ],
+    "id":2,
+    "schedules":[
+
+    ],
+    "withinSchedule":false,
+    "name":"Test 2",
+    "aliases":[
+
+    ],
+    "status":"MAINTENANCE",
+    "isAuto":false,
+    "isPublic":true,
+    "onShortList":true,
+    "serviceUrl":null,
+    "description":"<p>Hello, Test 2!</p>",
+    "projectId":null,
+    "type":"service"
+  },
+  {
+    "notes":[
+
+    ],
+    "id":3,
+    "schedules":[
+
+    ],
+    "withinSchedule":false,
+    "name":"Test 3",
+    "aliases":[
+
+    ],
+    "status":"UP",
+    "isAuto":false,
+    "isPublic":true,
+    "onShortList":true,
+    "serviceUrl":null,
+    "description":"<p>Hello, Test 3!</p>",
+    "projectId":null,
+    "type":"service"
+  }
+];
+
 angular.module('mock.serviceRepo', []).service('ServiceRepo', function ($q) {
 
   var ServiceRepo = this;
 
+  ServiceRepo.list = mockServices;
+
   ServiceRepo.create = function (service) {
     var defer = $q.defer();
+    service.id = ServiceRepo.list.length + 1;
+    ServiceRepo.list.push(service);
+    defer.resolve(service);
     return defer.promise;
   };
 
   ServiceRepo.update = function (service) {
     var defer = $q.defer();
+    for(var i in ServiceRepo.list) {
+      if(ServiceRepo.list[i].id === service.id) {
+        angular.extend(ServiceRepo.list[i], service);
+        service = ServiceRepo.list[i];
+        break;
+      }
+    }
+    defer.resolve(service);
     return defer.promise;
   };
 
   ServiceRepo.getAll = function () {
     var defer = $q.defer();
+    defer.resolve(ServiceRepo.list);
     return defer.promise;
+  };
+
+  ServiceRepo.findById = function(id) {
+    for(var i in ServiceRepo.list) {
+      if(ServiceRepo.list[i].id === id) {
+        return ServiceRepo.list[i];
+      }
+    }
   };
 
   ServiceRepo.ready = function () {
     var defer = $q.defer();
+    defer.resolve();
     return defer.promise;
   };
 
--- tests/unit/controllers/serviceControllerTest.js
+++ tests/unit/controllers/serviceControllerTest.js
@@ -1,6 +1,6 @@
 describe('controller: ServiceController', function () {
 
-  var scope, controller;
+  var scope, controller, ServiceRepo;
 
   beforeEach(module('core'));
 
@@ -13,6 +13,7 @@ describe('controller: ServiceController', function () {
   beforeEach(inject(function ($controller, $rootScope, _Service_, _ServiceRepo_) {
     installPromiseMatchers();
     scope = $rootScope.$new();
+    ServiceRepo = _ServiceRepo_;
     controller = $controller('ServiceController', {
       $scope: scope,
       Service: _Service_,
@@ -35,6 +36,65 @@ describe('controller: ServiceController', function () {
     it('updateService should be defined', function () {
       expect(scope.updateService).toBeDefined();
       expect(typeof scope.updateService).toEqual("function");
+    });
+  });
+
+  describe('Do the scope methods work as expected', function () {
+    it('createService should created a new project', function () {
+      var id = mockServices.length + 1;
+      var newService = {
+        "notes":[
+    
+        ],
+        "schedules":[
+    
+        ],
+        "withinSchedule":false,
+        "name":"Test 4",
+        "aliases":[
+    
+        ],
+        "status":"MAINTENANCE",
+        "isAuto":false,
+        "isPublic":true,
+        "onShortList":true,
+        "serviceUrl":null,
+        "description":"<p>Hello, Test 4!</p>",
+        "projectId":null,
+        "type":"service"
+      };
+      scope.serviceData = newService;
+      scope.createService();
+      newService.id = id;
+      expect(ServiceRepo.findById(id)).toEqual(newService);
+    });
+    it('updateService should update a project', function () {
+      var id = 1;
+      var updatedService = {
+        "notes":[
+    
+        ],
+        "id":id,
+        "schedules":[
+    
+        ],
+        "withinSchedule":false,
+        "name":"Test 1 Updated",
+        "aliases":[
+    
+        ],
+        "status":"MAINTENANCE",
+        "isAuto":true,
+        "isPublic":true,
+        "onShortList":true,
+        "serviceUrl":null,
+        "description":"<p>Hello, Test 1 Update!</p>",
+        "projectId":null,
+        "type":"service"
+      };
+      scope.serviceData = updatedService;
+      scope.updateService();
+      expect(ServiceRepo.findById(id)).toEqual(updatedService);
     });
   });
 
