8a17ebceccb3e38a3a0c3c0d887d13c68cf83e82
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2018-03-30 09:04:25 -0500

    Added edit and delete feature proposal functionallity

--- app/controllers/featureProposalController.js
+++ app/controllers/featureProposalController.js
@@ -4,12 +4,41 @@ app.controller('FeatureProposalController', function ($controller, $scope, Featu
     $scope: $scope
   }));
 
-  $scope.editFeatureProposal = function(fp) {
+  $scope.fpToDelete = {};
 
+  $scope.editFeatureProposal = function(fp) {
+    $scope.fpData = fp;
+    $scope.openModal('#editFpModal');
   };
 
   $scope.updateFeatureProposal = function(fp) {
+    FeatureProposalRepo.update($scope.fpData).then(function (res) {
+      if (angular.fromJson(res.body).meta.status === 'SUCCESS') {
+          $scope.resetFeatureProposals();
+      }
+    });
+  };
 
+  $scope.removeIdea = function(idea) {
+    if ($scope.fpData.ideas.some(function(i) {
+      return i.id === idea.id;
+    })) {
+      $scope.fpData.ideas.splice($scope.fpData.ideas.indexOf(idea), 1);
+    }
+  };
+
+  $scope.confirmDelete = function (fp) {
+    $scope.openModal('#deleteFpModal');
+    $scope.fpToDelete = fp;
+  };
+
+  $scope.deleteFp = function () {
+    $scope.deleting = true;
+    $scope.fpToDelete.delete().then(function () {
+      $scope.closeModal();
+      $scope.deleting = false;
+      $scope.fpToDelete = {};
+    });
   };
 
 });
