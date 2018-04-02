9c89432695631b2a03d951375e36bde701d8b6cb
William Welling <wwelling@library.tamu.edu>
2018-04-02 09:29:18 -0500

    table directive to cleanup controllers

--- app/controllers/ideaController.js
+++ app/controllers/ideaController.js
@@ -4,7 +4,7 @@ app.controller('IdeaController', function ($controller, $scope, $timeout, Featur
         $scope: $scope
     }));
 
-    $scope.ideaRepo = IdeaRepo;
+    $scope.repo = IdeaRepo;
 
     $scope.modalData = {
         title: '',
@@ -35,102 +35,6 @@ app.controller('IdeaController', function ($controller, $scope, $timeout, Featur
             property: 'lastModified'
         }
     ];
-
-    $scope.filter = $scope.filters[0];
-
-    $scope.activeFilters = IdeaRepo.getPageSettings().filters;
-
-    var activeSort = IdeaRepo.getPageSettings().sort = [{
-        property: 'service.name',
-        direction: 'ASC'
-    }, {
-        property: 'lastModified',
-        direction: 'DESC'
-    }];
-    
-    $scope.selectFilter = function(filter) {
-        $scope.filter = filter;
-    };
-
-    $scope.removeFilter = function (prop, v) {
-        $scope.activeFilters[prop].splice($scope.activeFilters[prop].indexOf(v), 1);
-        if($scope.activeFilters[prop].length === 0) {
-            delete $scope.activeFilters[prop];
-        }
-        IdeaRepo.getTableParams().reload();
-    };
-
-    $scope.applyFilter = function(filter) {
-        if($scope.activeFilters[filter.property]) {
-            $scope.activeFilters[filter.property].push(filter.value);
-        } else {
-            $scope.activeFilters[filter.property] = [filter.value];
-        }
-        IdeaRepo.getTableParams().reload();
-        delete $scope.filter.value;
-    };
-
-    $scope.lookupGloss = function(prop) {
-        for(var i in $scope.filters) {
-            var filter = angular.copy($scope.filters[i]);
-            if(filter.property === prop) {
-                return filter.gloss;
-            }
-        }
-    };
-
-    $scope.unsorted = function(prop) {
-        for(var i in activeSort) {
-            var sort = activeSort[i];
-            if(sort.property === prop) {
-                return false;
-            }
-        }
-        return true;
-    };
-
-    $scope.asc = function(prop) {
-        for(var i in activeSort) {
-            var sort = activeSort[i];
-            if(sort.property === prop && sort.direction === 'ASC') {
-                return true;
-            }
-        }
-        return false;
-    };
-
-    $scope.desc = function(prop) {
-        for(var i in activeSort) {
-            var sort = activeSort[i];
-            if(sort.property === prop && sort.direction === 'DESC') {
-                return true;
-            }
-        }
-        return false;
-    };
-
-    $scope.toggleSort = function(prop) {
-        var asc = true;
-        for(var i in activeSort) {
-            var sort = activeSort[i];
-            if(sort.property === prop) {
-                if(sort.direction === 'ASC') {
-                    sort.direction = 'DESC';
-                } else {
-                    activeSort.splice(i, 1);
-                }
-                asc = false;
-                break;
-            }
-        }
-        if(asc) {
-            activeSort.push({
-                property: prop,
-                direction: 'ASC'
-            });
-        }
-        IdeaRepo.getTableParams().reload();
-    };
 
     ServiceRepo.ready().then(function () {
 
--- app/controllers/noteController.js
+++ app/controllers/noteController.js
@@ -4,7 +4,7 @@ app.controller('NoteController', function ($controller, $scope, Note, NoteRepo, 
         $scope: $scope
     }));
 
-    $scope.noteRepo = NoteRepo;
+    $scope.repo = NoteRepo;
 
     $scope.services = ServiceRepo.getAll();
 
@@ -73,102 +73,6 @@ app.controller('NoteController', function ($controller, $scope, Note, NoteRepo, 
             property: 'active'
         }
     ];
-    
-    $scope.filter = $scope.filters[0];
-
-    $scope.activeFilters = NoteRepo.getPageSettings().filters;
-
-    var activeSort = NoteRepo.getPageSettings().sort = [{
-        property: 'service.name',
-        direction: 'ASC'
-    }, {
-        property: 'lastModified',
-        direction: 'DESC'
-    }];
-    
-    $scope.selectFilter = function(filter) {
-        $scope.filter = filter;
-    };
-    
-    $scope.removeFilter = function (prop, v) {
-        $scope.activeFilters[prop].splice($scope.activeFilters[prop].indexOf(v), 1);
-        if($scope.activeFilters[prop].length === 0) {
-            delete $scope.activeFilters[prop];
-        }
-        NoteRepo.getTableParams().reload();
-    };
-    
-    $scope.applyFilter = function(filter) {
-        if($scope.activeFilters[filter.property]) {
-            $scope.activeFilters[filter.property].push(filter.value);
-        } else {
-            $scope.activeFilters[filter.property] = [filter.value];
-        }
-        NoteRepo.getTableParams().reload();
-        delete $scope.filter.value;
-    };
-
-    $scope.lookupGloss = function(prop) {
-        for(var i in $scope.filters) {
-            var filter = angular.copy($scope.filters[i]);
-            if(filter.property === prop) {
-                return filter.gloss;
-            }
-        }
-    };
-    
-    $scope.unsorted = function(prop) {
-        for(var i in activeSort) {
-            var sort = activeSort[i];
-            if(sort.property === prop) {
-                return false;
-            }
-        }
-        return true;
-    };
-    
-    $scope.asc = function(prop) {
-        for(var i in activeSort) {
-            var sort = activeSort[i];
-            if(sort.property === prop && sort.direction === 'ASC') {
-                return true;
-            }
-        }
-        return false;
-    };
-    
-    $scope.desc = function(prop) {
-        for(var i in activeSort) {
-            var sort = activeSort[i];
-            if(sort.property === prop && sort.direction === 'DESC') {
-                return true;
-            }
-        }
-        return false;
-    };
-    
-    $scope.toggleSort = function(prop) {
-        var asc = true;
-        for(var i in activeSort) {
-            var sort = activeSort[i];
-            if(sort.property === prop) {
-                if(sort.direction === 'ASC') {
-                    sort.direction = 'DESC';
-                } else {
-                    activeSort.splice(i, 1);
-                }
-                asc = false;
-                break;
-            }
-        }
-        if(asc) {
-            activeSort.push({
-                property: prop,
-                direction: 'ASC'
-            });
-        }
-        NoteRepo.getTableParams().reload();
-    };
 
     ServiceRepo.ready().then(function () {
 
