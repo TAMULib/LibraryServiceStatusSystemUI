c6848ba517c850124586c4ddad82438f5eaa3c45
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2018-06-25 14:39:09 -0500

    Refactored visibility login into management controller

--- app/controllers/managementController.js
+++ app/controllers/managementController.js
@@ -4,4 +4,27 @@ app.controller('ManagementController', function ($controller, $scope) {
         $scope: $scope
     }));
 
+    $scope.canManageServices = function () {
+        return ($scope.hasAdminAccess() || $scope.isServiceManager());
+    };
+
+    $scope.canManageNotes = function () {
+        return ($scope.hasManagementAccess() && !$scope.isNoticeManager());
+    };
+
+    $scope.canManageIdeas = function () {
+        return ($scope.hasAdminAccess() || $scope.isServiceManager());
+    };
+
+    $scope.canManageFeatureProposals = function () {
+        return ($scope.hasAdminAccess() || $scope.isServiceManager());
+    };
+
+    $scope.canManageNotifications = function () {
+        return ($scope.hasAdminAccess() || $scope.isWebManager() || $scope.isNoticeManager());
+    };
+
+    $scope.canManageUsers = function () {
+        return $scope.isAdmin();
+    };
 });
