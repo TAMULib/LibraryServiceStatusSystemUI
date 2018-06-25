4ce8b8e90a1c90fe432488670cb92ac20a7932c8
Kevin Day <kday@library.tamu.edu>
2018-06-25 09:40:57 -0500

    Item b03526: Present tabs based on roles, using ng-show
    Perform access control in the controller and present a list of columns for renderring such than an ng-repeat can be used:
      `<wvr-tab ng-repeat="column in columns" path="{{column.path}}" view="views/management/{{column.view}}.html" label="{{column.label}}">{{column.label}}</wvr-tab>`
    
    This is not working as expected, possibly due to the current weaver tabs implementation.
    This includes a not so great work-around using ng-show with a preset number of columns.
    
    This should be improved to use ng-repeat or something similar instead of using ng-show with a preset column list.

--- app/controllers/managementController.js
+++ app/controllers/managementController.js
@@ -1,7 +1,81 @@
-app.controller('ManagementController', function ($controller, $scope) {
-
+app.controller('ManagementController', function ($controller, $scope, UserService) {
     angular.extend(this, $controller('AbstractController', {
         $scope: $scope
     }));
 
+    var columns = {
+      "services": {
+        path: "services",
+        view: "services",
+        label: "Services"
+      },
+      "notes": {
+        path: "notes",
+        view: "notes",
+        label: "Notes"
+      },
+      "ideas": {
+        path: "ideas",
+        view: "ideas",
+        label: "Ideas"
+      },
+      "featureProposals": {
+        path: "feature-proposals",
+        view: "featureProposals",
+        label: "Feature Proposals"
+      },
+      "notifications": {
+        path: "notifications",
+        view: "notifications",
+        label: "Notifications"
+      },
+      "users": {
+        path: "users",
+        view: "users",
+        label: "Users"
+      }
+    };
+
+    $scope.user = UserService.getCurrentUser();
+
+    $scope.columns = [];
+
+    if ($scope.user.role == "ROLE_ADMIN") {
+        $scope.columns = [
+            columns.services,
+            columns.notes,
+            columns.ideas,
+            columns.featureProposals,
+            columns.notifications,
+            columns.users
+        ];
+    }
+    else if ($scope.user.role == "ROLE_SERVICE_ADMIN") {
+        $scope.columns = [
+            columns.services,
+            columns.notes,
+            columns.ideas,
+            columns.featureProposals,
+            columns.notifications
+        ];
+    }
+    else if ($scope.user.role == "ROLE_SERVICE_MANAGER") {
+        $scope.columns = [
+            columns.services,
+            columns.notes,
+            columns.ideas,
+            columns.featureProposals,
+        ];
+    }
+    else if ($scope.user.role == "ROLE_WEB_MANAGER") {
+        $scope.columns = [
+            columns.notes,
+            columns.notifications
+        ];
+    }
+    else if ($scope.user.role == "ROLE_NOTICE_MANAGER") {
+        $scope.columns = [
+            columns.notifications
+        ];
+    }
 });
