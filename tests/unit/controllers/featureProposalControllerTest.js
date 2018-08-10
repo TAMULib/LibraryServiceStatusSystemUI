809b1778c9a7916427bb61de416d60a23ec8782c
Kevin Day <kday@library.tamu.edu>
2018-08-10 12:45:39 -0500

    Rewrite junit tests on FeatureProposalController to support IdeaState and pass missing parameters

--- tests/unit/controllers/featureProposalControllerTest.js
+++ tests/unit/controllers/featureProposalControllerTest.js
@@ -6,22 +6,26 @@ describe('controller: FeatureProposalController', function () {
         module('core');
         module('app');
         module('mock.idea');
-        module('mock.featureProposal');
-        module('mock.projectService');
-        module('mock.featureProposalRepo');
         module('mock.ideaRepo');
+        module('mock.projectService');
+        module('mock.featureProposal');
+        module('mock.featureProposalRepo');
         module('mock.serviceRepo');
 
-        inject(function ($controller, $rootScope, _Idea_, _FeatureProposal_, FeatureProposalState, _ProjectService_, _FeatureProposalRepo_, _IdeaRepo_, _ServiceRepo_) {
+        inject(function ($controller, $rootScope, _Idea_, _IdeaRepo_, IdeaState, _FeatureProposal_, _FeatureProposalRepo_, FeatureProposalState, _ProjectService_, _ServiceRepo_) {
             installPromiseMatchers();
             scope = $rootScope.$new();
             fps = FeatureProposalState;
             controller = $controller('FeatureProposalController', {
                 $scope: scope,
                 Idea: _Idea_,
+                IdeaRepo: _IdeaRepo_,
+                IdeaState: IdeaState,
                 FeatureProposal: _FeatureProposal_,
+                FeatureProposalRepo: _FeatureProposalRepo_,
                 FeatureProposalState: FeatureProposalState,
-                ProjectService: _ProjectService_
+                ProjectService: _ProjectService_,
+                ServiceRepo: _ServiceRepo_
             });
         });
     });
