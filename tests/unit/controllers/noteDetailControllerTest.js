67f452df4b8d4de3c62c5703fa899d913534779b
Kevin Day <kday@library.tamu.edu>
2018-08-17 13:02:26 -0500

    Add missing NoteDetailController test
    I failed to write the controller test for NoteDetailController.
    This adds it and checks to see if $scope.note is properly initialized.

---
+++ tests/unit/controllers/noteDetailControllerTest.js
@@ -0,0 +1,33 @@
+describe('controller: NoteDetailController', function () {
+
+    var controller, scope, routeParams, NoteRepo;
+
+    beforeEach(function() {
+        module('core');
+        module('app');
+        module('mock.note');
+        module('mock.noteRepo');
+
+        inject(function ($controller, $rootScope, _Note_, _NoteRepo_) {
+            installPromiseMatchers();
+            scope = $rootScope.$new();
+            routeParams = {
+                noteId: 123456789
+            };
+            NoteRepo = _NoteRepo_;
+            controller = $controller('NoteDetailController', {
+                $scope: scope,
+                $routeParams: routeParams,
+                Note: _Note_,
+                NoteRepo: _NoteRepo_
+            });
+        });
+    });
+
+    describe('Is the controller defined', function () {
+        it('should be defined', function () {
+            expect(controller).toBeDefined();
+        });
+    });
+
+});
